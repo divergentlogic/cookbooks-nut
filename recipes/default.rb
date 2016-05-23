909d57f26dac1da1a03aa4aca8d32f7920d7c791
Tim Smith <tsmith@chef.io>
2016-05-22 21:42:06 -0700

    Resolve all cookstyle warnings
    Cookstyle is the cookbook version of Rubocop. It has rules that aren't
    applicable disabled
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,86 +19,82 @@
 # limitations under the License.
 #
 
-package "nut"
+package 'nut'
 # Precise creates the user and group called 'nut'
-package "nut-snmp"
+package 'nut-snmp'
 
-service "nut-server" do
-  if node["platform_version"].to_f >= 14.04
-    service_name "nut-server"
+service 'nut-server' do
+  if node['platform_version'].to_f >= 14.04
+    service_name 'nut-server'
   else
-    service_name "nut"
+    service_name 'nut'
   end
-  supports :start => true, :stop => true, :reload => true, :restart => true, :status => true
+  supports start: true, stop: true, reload: true, restart: true, status: true
 end
 
-
-service "nut-client" do
-  if node["platform_version"].to_f >= 14.04
-    service_name "nut-client"
+service 'nut-client' do
+  if node['platform_version'].to_f >= 14.04
+    service_name 'nut-client'
   else
-    service_name "nut"
+    service_name 'nut'
   end
-  supports :start => true, :stop => true, :reload => true, :restart => true, :status => true
+  supports start: true, stop: true, reload: true, restart: true, status: true
 end
 
-template "/etc/udev/rules.d/53-nut-serialups.rules" do
-  source "nut-serialups.rules.erb"
-  owner "root"
-  group "root"
+template '/etc/udev/rules.d/53-nut-serialups.rules' do
+  source 'nut-serialups.rules.erb'
+  owner 'root'
+  group 'root'
   mode 0644
   not_if { node['nut']['devices'].empty? }
 end
 
-template "/etc/nut/nut.conf" do
-  source "nut.conf.erb"
-  owner "root"
-  group "root"
+template '/etc/nut/nut.conf' do
+  source 'nut.conf.erb'
+  owner 'root'
+  group 'root'
   mode 0644
   case node['nut']['mode']
-  when "netserver"
-    unless node['nut']['monitors'].nil?
-      notifies :restart, 'service[nut-client]'
-    end
+  when 'netserver'
+    notifies :restart, 'service[nut-client]' unless node['nut']['monitors'].nil?
     notifies :restart, 'service[nut-server]'
-  when "netclient"
+  when 'netclient'
     notifies :restart, 'service[nut-client]'
-  when "standalone"
+  when 'standalone'
     notifies :restart, 'service[nut-client]'
     notifies :restart, 'service[nut-server]'
   end
 end
 
-template "/etc/nut/ups.conf" do
-  source "ups.conf.erb"
-  owner "root"
-  group "nut"
+template '/etc/nut/ups.conf' do
+  source 'ups.conf.erb'
+  owner 'root'
+  group 'nut'
   mode 0640
   notifies :reload, 'service[nut-server]'
   not_if { node['nut']['ups'].empty? }
 end
 
-template "/etc/nut/upsd.conf" do
-  source "upsd.conf.erb"
-  owner "root"
-  group "nut"
+template '/etc/nut/upsd.conf' do
+  source 'upsd.conf.erb'
+  owner 'root'
+  group 'nut'
   mode 0640
   notifies :reload, 'service[nut-server]'
 end
 
-
-template "/etc/nut/upsd.users" do
-  source "upsd.users.erb"
-  owner "root"
-  group "nut"
+template '/etc/nut/upsd.users' do
+  source 'upsd.users.erb'
+  owner 'root'
+  group 'nut'
   mode 0640
   notifies :reload, 'service[nut-server]'
 end
 
-template "/etc/nut/upsmon.conf" do
-  source "upsmon.conf.erb"
-  owner "root"
-  group "nut"
+template '/etc/nut/upsmon.conf' do
+  source 'upsmon.conf.erb'
+  owner 'root'
+  group 'nut'
   mode 0640
   notifies :reload, 'service[nut-client]'
 end
@@ -106,25 +102,25 @@ end
 # Starting service correspond to nut.conf mode
 case node['nut']['mode']
 
-  when "netserver"
-    service "nut-client" do
-      action [ :enable, :start ]
-      not_if { node['nut']['monitors'].nil? }
-    end
-    service "nut-server" do
-      action [ :enable, :start ]
-    end
-
-  when "netclient"
-    service "nut-client" do
-      action [ :enable, :start ]
-    end
-
-  when "standalone"
-    service "nut-client" do
-      action [ :enable, :start ]
-    end
-    service "nut-server" do
-      action [ :enable, :start ]
-    end
+when 'netserver'
+  service 'nut-client' do
+    action [:enable, :start]
+    not_if { node['nut']['monitors'].nil? }
   end
+  service 'nut-server' do
+    action [:enable, :start]
+  end
+
+when 'netclient'
+  service 'nut-client' do
+    action [:enable, :start]
+  end
+
+when 'standalone'
+  service 'nut-client' do
+    action [:enable, :start]
+  end
+  service 'nut-server' do
+    action [:enable, :start]
+  end
+end
